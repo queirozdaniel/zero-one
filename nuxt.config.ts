7538d9de64a9d7d7f9a996c6859f62e323df3132
queirozdaniel <danielqueirozak77@gmail.com>
2025-03-27 20:46:22 -0300

    feat: All initial configs

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+
+  googleFonts: {
+    base64: true,
+    fontsDir: 'assets/fonts',
+    overwriting: true,
+    families: {
+      Inter: [300, 500, 800],
+    },
+  },
+
+  compatibilityDate: '2025-03-27',
+})
