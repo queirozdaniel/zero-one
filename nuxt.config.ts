e487475d560a10fd03234a8237c98c4860a25525
queirozdaniel <danielqueirozak77@gmail.com>
2025-04-18 17:52:24 -0300

    chore: added @vue-use and @vue-query

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@hebilicious/vue-query-nuxt'],
 
   googleFonts: {
     base64: true,
